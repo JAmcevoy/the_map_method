c18897ea6443172b5d492b6ec2ef894b4dc2b988
Joshua McEvoy <144149195+JAmcevoy@users.noreply.github.com>
2024-02-27 10:29:37 +0000

    Example done

--- map.js
+++ map.js
@@ -13,12 +13,24 @@ for(let num of nums) {
 console.log(results);
 
 // Using map()
+const multByTwo = function (num){
+  return num * 2;
+}
+
+const mapResults = nums.map(multByTwo);
+console.log(mapResults);
 
 
 // Simplified w/ map()
+const simplfied = nums.map(function (num){return num * 2}); //----Function has been moved in brackets to make single line
+console.log(simplfied)
 
 
 // Simplfied w/ map() + arrow function
+const arrow = nums.map(num => num * 2); 
+//----It only takes one parameter, the number to be multiplied by two, so it doesn’t need the parentheses around num  here.                               
+//----The return value is quite simple too, so it doesn’t even need the return statement,  parentheses or curly braces.  
+console.log(arrow);
 
 
 // With objects:
@@ -42,3 +54,8 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+const stundentWithIds = students.map(student => [student.name, student.id, student.skill]); 
+//---The student before the arrow is declared as the single then using the dot notation I can pull each obeject from the map
+//--- Inside the '[]' make it an array
+console.log(stundentWithIds);
